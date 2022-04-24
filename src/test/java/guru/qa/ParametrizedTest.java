34e1a1b1921409efe1b2390805952b6a903fddd9
VEvtushenko <evt-vladimir@yandex.ru>
2022-04-24 22:48:25 +0300

    GOG test

--- src/test/java/guru/qa/ParametrizedTest.java
+++ src/test/java/guru/qa/ParametrizedTest.java
@@ -6,8 +6,11 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -39,5 +42,22 @@ public class ParametrizedTest {
         $("div.menu-search__results-rows").$("div.menu-search__result").click();
         $(".age-gate__button").click();
         $("h1.productcard-basics__title").shouldHave(Condition.text(checkedData));
+    }
+
+    static Stream<Arguments> testGOGWithSource() {
+        return Stream.of(
+                Arguments.of("Baldur's Gate", "Baldur's Gate"),
+                Arguments.of("The Elder Scrolls", "The Elder Scrolls IV: Oblivion")
+        );
+    }
+
+    @MethodSource("testGOGWithSource")
+    @ParameterizedTest(name = "Поиск на сайте GOG игр серии {0}, проверка, что первая в списке {1}")
+    void testGOGWithSource(String gameName, String gameVersion) {
+        Selenide.open("https://www.gog.com/ru");
+        $("svg.menu-icon-svg--search").click();
+        $(".menu-search-input__field").setValue(gameName);
+        $("div.menu-search__results-rows").$("div.menu-search__result").click();
+        $("h1.productcard-basics__title").shouldHave(Condition.text(gameVersion));
     }
 }
